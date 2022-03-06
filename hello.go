9c3d0d2c925c8b4c669dfa705055a7dd124cf50c
rikirenz <riccardo.candido@gmail.com>
2022-03-06 11:09:50 +0100

    simple hello world function with tests
    Signed-off-by: rikirenz <riccardo.candido@gmail.com>

--- hello.go
+++ hello.go
@@ -2,10 +2,33 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name
+const spanish = "Spanish"
+const french = "French"
+const frenchHelloPrefix = "Bonjur, "
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
 }
 
+func greetingPrefix(language string) (prefix string){
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix =  englishHelloPrefix
+	}
+	return
+}
+
+
 func main() {
-	fmt.Println(Hello("Riki"))
+	fmt.Println(Hello("Riki", ""))
 }
